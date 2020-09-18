b64fd14828947b182540ce302b78a9bd59172c53
shishuozheng <1697218933@qq.com>
2020-09-18 17:30:30 +0800

    修改配置

--- inc_config_online.php
+++ inc_config_online.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
-    'api'=>'https://www.staffhome.cn/api/',
-    'wenti_domian'=>'http://wentizhichuang.cn',//文体之窗域名
+    'api'=>'https://api.staffhome.cn/api/',
+    'wenti_domian'=>'http://wt.staffhome.cn/',//文体之窗域名
     'jianbu_domian'=>'http://jianbuzou.cn',//健步走域名
     'club_domian'=>'http://club.cn',//文体之窗域名
 ];
