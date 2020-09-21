230f0f6151752515837f10b3c993623109e07926
shishuozheng <1697218933@qq.com>
2020-09-21 11:13:08 +0800

    健步走域名

--- inc_config_online.php
+++ inc_config_online.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
     'api'=>'https://api.staffhome.cn/',
-    'wenti_domian'=>'http://wt.staffhome.cn/',//文体之窗域名
-    'jianbu_domian'=>'http://jianbuzou.cn',//健步走域名
+    'wenti_domian'=>'http://wt.staffhome.cn',//文体之窗域名
+    'jianbu_domian'=>'http://step.staffhome.cn',//健步走域名
     'club_domian'=>'http://club.cn',//文体之窗域名
 ];
