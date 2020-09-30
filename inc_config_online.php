a9cab27dbbe27933d737bd74e0095f56239cf319
shishuozheng <1697218933@qq.com>
2020-09-30 13:54:28 +0800

    线上配置

--- inc_config_online.php
+++ inc_config_online.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
     'api'=>'https://api.staffhome.cn/',
-    'wenti_domian'=>'http://wt.staffhome.cn',//文体之窗域名
-    'jianbu_domian'=>'http://step.staffhome.cn',//健步走域名
-    'club_domian'=>'http://club.cn',//文体之窗域名
+    'wenti_domian'=>'http://wt.wenyesports.cn',//文体之窗域名
+    'jianbu_domian'=>'http://walk.wenyesports.cn',//健步走域名
+    'club_domian'=>'http://club.wenyesports.cn',//文体之窗域名
 ];
