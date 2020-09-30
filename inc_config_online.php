8723e7e2d20200b10ff7b101cac146542560a57f
shishuozheng <1697218933@qq.com>
2020-09-30 16:43:55 +0800

    配置

--- inc_config_online.php
+++ inc_config_online.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
     'api'=>'https://api.staffhome.cn/',
-    'wenti_domian'=>'http://wt.wenyesports.cn',//文体之窗域名
-    'jianbu_domian'=>'http://walk.wenyesports.cn',//健步走域名
-    'club_domian'=>'http://club.wenyesports.cn',//文体之窗域名
+    'wenti_domian'=>'http://wt.wenyesports.com',//文体之窗域名
+    'jianbu_domian'=>'http://walk.wenyesports.com',//健步走域名
+    'club_domian'=>'http://club.wenyesports.com',//文体之窗域名
 ];
